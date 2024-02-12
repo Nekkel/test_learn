4a1ce7c7a351a28569bacf48e48d873f75cb440a
Nekkel <nekkel1589@yandex.ru>
2024-02-12 07:58:01 +0300

    fix

--- test_s-d.py
+++ test_s-d.py
@@ -17,46 +17,40 @@ def wait_of_element_located(xpath, driver):
     return element
 
 
-def test_add_jacket_to_the_shopcart():
+def test_add_bbrfactory_com():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     driver = webdriver.Chrome(options=options)
     driver.get("https://shop.bbrfactory.com/")
 
-    # Поиск и ожидание элементов и присваивание к переменным.
-   # input_username = wait_of_element_located(xpath='//*[@id=\"user-name\"]', driver=driver)
-    # input_password = wait_of_element_located(xpath='//*[@id=\"password\"]', driver=driver)
-    # login_button = wait_of_element_located(xpath='//*[@id=\"login-button\"]', driver=driver)
+    # Setup chrome driver
+    driver.set_window_size(1920, 1080)
 
-    # Действия с формами
-   # input_username.send_keys("standard_user")
-  # input_password.send_keys("secret_sauce")
-   # login_button.send_keys(Keys.RETURN)
 
     # Поиск и ождиание прогрузки ссылки элемента товара магазина и клик по ссылке
+
     item_name = wait_of_element_located(xpath='//*[@id="main-nav"]/ul/li[2]/a', driver=driver)
     item_name.click()
 
     # Поиск и ождиание прогрузки ссылки элемента товара магазина и клик по ссылке
-    item_name = wait_of_element_located(xpath='//*[@id="main-nav"]/ul/li[2]/a', driver=driver)
+    item_name = wait_of_element_located(xpath='//*[@id="categories-wrapper"]/ul/li[5]/a', driver=driver)
     item_name.click()
 
-    # Поиск и ожидание кнопки добавления товара и клик по этой кнопке
-    item_add_button = wait_of_element_located(xpath='//*[@id=\"add-to-cart-sauce-labs-fleece-jacket\"]', driver=driver)
-    item_add_button.click()
+    # Поиск и ождиание прогрузки ссылки элемента товара магазина и клик по ссылке
+    item_name = wait_of_element_located(xpath='//*[@title="Джим Вернулся 254"]', driver=driver)
+    item_name.click()
 
-    # Ждем пока товар добавится в корзину, появится span(кол-во позиций в корзине) и кликаем по корзине чтобы перейти
-    wait_of_element_located(xpath='//*[@id=\"shopping_cart_container\"]/a/span', driver=driver).click()
+    # Поиск и ождиание прогрузки ссылки элемента товара магазина и клик по ссылке
+    item_name = wait_of_element_located(xpath='//*[@id="button-cart"]', driver=driver)
+    item_name.click()
 
-    # Еще один поиск ссылки элемента позиции магазина
-    item_name = wait_of_element_located(xpath='//*[@id=\"item_5_title_link\"]/div', driver=driver)
-    if item_name.text == "Sauce Labs Fleece Jacket":
-        print("Товар пристутствует в корзине")
-    else:
-        print("Товар отсутствует")
+    # Поиск и ождиание прогрузки ссылки элемента товара магазина и клик по ссылке
+    item_name = wait_of_element_located(xpath='//*[@id="modalAdded"]/div/div/div[2]/a', driver=driver)
+    item_name.click()
+
 
     time.sleep(5)
 
 
 if __name__ == '__main__':
-    test_add_jacket_to_the_shopcart()
+    test_add_bbrfactory_com()
